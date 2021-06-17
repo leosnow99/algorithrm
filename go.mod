4500f46dfa6e538d048eaa288e6de1a5a96235d5
lipenghui.leosnow <lipenghui.leosnow@bytedance.com>
2021-06-17 23:42:34 +0800

    day:21-6-17

--- go.mod
+++ go.mod
@@ -3,16 +3,6 @@ module algorithm
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/kataras/golog v0.1.2 // indirect
-	github.com/kataras/iris v0.0.1 // indirect
-	github.com/klauspost/compress v1.10.11 // indirect
-	github.com/microcosm-cc/bluemonday v1.0.4 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/ryanuber/columnize v2.1.2+incompatible // indirect
-	golang.org/x/net v0.0.0-20200813134508-3edf25e44fcc // indirect
-	golang.org/x/sys v0.0.0-20200819171115-d785dc25833f // indirect
-	google.golang.org/appengine v1.6.6 // indirect
-	gopkg.in/ini.v1 v1.60.0 // indirect
+	github.com/ahrtr/gocontainer v0.1.0
+	github.com/deckarep/golang-set v1.7.1
 )
---
+++ src/string_type/string_path_convert.go
@@ -0,0 +1,76 @@
+package string_type
+
+import set "github.com/deckarep/golang-set"
+import "github.com/ahrtr/gocontainer/list"
+
+func getNexts(words []string) map[string][]string {
+	dict := set.NewSet(words)
+	nexts := make(map[string][]string)
+
+	for _, w := range words {
+		nexts[w] = getNext(w, dict)
+	}
+	return nexts
+}
+
+func getNext(word string, dict set.Set) (res []string) {
+	chs := []byte(word)
+	for cur := byte('a'); cur < 'z'; cur++ {
+		for i := 0; i < len(chs); i++ {
+			if chs[i] != cur {
+				tmp := chs[i]
+				chs[i] = cur
+				if dict.Contains(string(chs)) {
+					res = append(res, string(chs))
+				}
+				chs[i] = tmp
+			}
+		}
+	}
+	return
+}
+
+func getDistance(start string, nexts map[string][]string) map[string]int {
+	var distance = make(map[string]int)
+	distance[start] = 0
+	var queue = list.NewLinkedList()
+	var sets = set.NewSet()
+	sets.Add(start)
+	for !queue.IsEmpty() {
+		v, _ := queue.Get(0)
+		cur := v.(string)
+		for _, str := range nexts[cur] {
+			if !sets.Contains(str) {
+				distance[str] = distance[cur] + 1
+				queue.Add(str)
+				sets.Add(str)
+			}
+		}
+	}
+	return distance
+}
+
+func getShortestPaths(cur, to string, nexts map[string][]string, distance map[string]int, solution []string,
+	res [][]string) {
+	solution = append(solution, cur)
+	if to == cur {
+		res = append(res, solution)
+	} else {
+		for _, next := range nexts[cur] {
+			if distance[next] == distance[cur]+1 {
+				getShortestPaths(next, to, nexts, distance, solution, res)
+			}
+		}
+	}
+	solution = solution[:len(solution)-1]
+}
+
+func findMinPaths(start, to string, lists []string) [][]string {
+	lists = append(lists, start)
+	nexts := getNexts(lists)
+	distance := getDistance(start, nexts)
+	var pathList []string
+	var res [][]string
+	getShortestPaths(start, to, nexts, distance, pathList, res)
+	return res
+}
